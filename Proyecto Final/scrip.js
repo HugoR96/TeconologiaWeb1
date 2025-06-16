315c97918b74cf9e40880d7c5f2779ad27a349eb
HugoR96 <162131124+HugoR96@users.noreply.github.com>
2025-06-16 10:06:59 -0400

    Proyecto Final

---
+++ Proyecto Final/scrip.js
@@ -0,0 +1,25 @@
+const motos = [
+    {
+      marca: "Yamaha",
+      modelo: "MT-07",
+      cilindrada: "689cc",
+      precio: 8700,
+      imagen: " "
+    },
+    {
+      marca: "Honda",
+      modelo: "CB500F",
+      cilindrada: "471cc",
+      precio: 6500,
+      imagen: "https://upload.wikimedia.org/wikipedia/commons/f/f5/CB500F_white.jpg"
+    },
+    {
+      marca: "Kawasaki",
+      modelo: "Z900",
+      cilindrada: "948cc",
+      precio: 9400,
+      imagen: "https://upload.wikimedia.org/wikipedia/commons/f/fb/Kawasaki_Z900_2017.jpg"
+    }
+  ];
+  
+  
